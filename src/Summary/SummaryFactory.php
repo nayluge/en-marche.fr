ceb793794f6812a6193bbbc64aea7771e1f4b48e
illusionOfParadise <olga.rotach@gmail.com>
2017-06-21 15:12:01 +0200

    Add fixtures for CV page

--- src/Summary/SummaryFactory.php
+++ src/Summary/SummaryFactory.php
@@ -19,4 +19,33 @@ class SummaryFactory
     {
         return Summary::createFromMember($adherent, $this->slugger->slugify($adherent->getFullName()));
     }
+
+    /**
+     * Used for test and loadiang fixtures purpose.
+     *
+     * @param array $data
+     * 
+     * @return Summary
+     */
+    public function createFromArray(array $data): Summary
+    {
+        $admin = new Summary($data['adherent'], $data['slug']);
+
+        $admin->setAvailability($data['availability']);
+        $admin->setContactEmail($data['contactEmail']);
+        $admin->setContributionWish($data['contributionWish']);
+        $admin->setJobLocation($data['jobLocation']);
+        $admin->setMotivation($data['motivation']);
+        $admin->setProfessionalSynopsis($data['professionalSynopsis']);
+
+        $admin->setCurrentProfession($data['currentProfession'] ?? '');
+        $admin->setLinkedInUrl($data['email'] ?? '');
+        $admin->setWebsiteUrl($data['websiteUrl'] ?? '');
+        $admin->setViadeoUrl($data['viadeoUrl'] ?? '');
+        $admin->setFacebookUrl($data['facebookUr'] ?? '');
+        $admin->setTwitterNickname($data['twitterNickname'] ?? '');
+        $admin->setMissionTypeWishes($data['missionWishes'] ?? []);
+
+        return $admin;
+    }
 }
